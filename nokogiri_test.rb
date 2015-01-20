50f4110bfbd64d0763c6de8f90ed00de7113a51b
Sergey <biginf.com@gmail.com>
2015-01-21 01:32:03 +0300

    work withe out img

--- nokogiri_test.rb
+++ nokogiri_test.rb
@@ -7,15 +7,41 @@ require 'csv'
 html = Nokogiri::HTML(open("http://www.viovet.co.uk/Pet_Foods_Diets-Dogs-Hills_Pet_Nutrition-%20Hills_Prescription_Diets/c233_234_2678_93/category.html"))
 
 html.xpath('//a[@class="product-content-box panel"]/@href').each do |node|
-       #puts "http://www.viovet.co.uk#{node.text}"
+
        url = "http://www.viovet.co.uk#{node.text}"
        puts url
 
        products = Nokogiri::HTML(open("#{url}"))
-       products.xpath('//h1').each do |title|
-       		puts title.text
+       		products.xpath('//li[@itemprop="offers"]').each do |node|
+       			section_html = Nokogiri::HTML(node.inner_html)
+       			title = section_html.xpath('//div[@class="title"]').text
+       			price = section_html.xpath('//span[@itemprop="price"]').text
+       			sku = section_html.xpath('//strong[@itemprop="sku"]').text
+       			dispatch = section_html.xpath('//strong[@class="stock in-stock"]').text
+
+       			puts title
+       			puts price
+       			puts sku
+       			puts dispatch
+       			puts "\n\n\n"
+
+       			
+       			#title = product_info.xpath('//div[@class="title"]').text
+       			#price = product_info.xpath('//span[@itemprop="price"]').text
+       			#puts title
+       			#puts price
        end
 
+
+#       products.xpath('//div[@class="title"]').each do |title|
+ #      		title = title.text
+  #     		puts title		
+   #    end
+#
+ #      products.xpath('//span[@itemprop="price"]').each do |price|
+  #     		price = price.text
+   #    		puts price
+    #   end
     end
 
 
